a31ba3098f5b5bf2b4a61ecdb3a725d8d9fbc972
DavidWoo <mengxuanwu57@gmail.com>
2026-04-24 02:00:07 -0700

    Initial Commit

---
+++ Pannotate-iOS/Pannotate_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pannotate_iOSApp.swift
+//  Pannotate-iOS
+//
+//  Created by DavidWoo on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Pannotate_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pannotate-iOSTests/Pannotate_iOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  Pannotate_iOSTests.swift
+//  Pannotate-iOSTests
+//
+//  Created by DavidWoo on 4/24/26.
+//
+
+import Testing
+@testable import Pannotate_iOS
+
+struct Pannotate_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
